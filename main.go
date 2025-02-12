cb8388828316a1e44bac28c787d931d336f378bf
YaguarEgor <Egor072005@yandex.ru>
2025-02-12 23:29:03 +0300

    DB added

--- main.go
+++ main.go
@@ -1,38 +1,52 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
-
+	"os"
 	"sync/atomic"
+
+	"github.com/YaguarEgor/chirpy_server/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
 	const filepathRoot = "."
 	const port = "8080"
 
+	db_url := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", db_url)
+	if err != nil {
+		log.Fatalf("error when opening db")
+	}
+	dbQueries := database.New(db)
+
 	//serverMux := http.ServeMux{}
-	apiCfg := apiConfig {
+	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
 	}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app",  apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerCounts)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidation)
 	server := &http.Server{
 		Handler: mux,
-		Addr:    ":"+port,
+		Addr:    ":" + port,
 	}
 	server.ListenAndServe()
 
 }
-
-
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
